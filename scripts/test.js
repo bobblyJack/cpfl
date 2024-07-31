db12d11922c26db85841d38f30d1c4c89e8ca5ce
LG <lg@cpfl.com.au>
2024-07-31 15:51:38 +0930

    cleaned up mistakes in manifest and html

--- scripts/test.js
+++ scripts/test.js
@@ -1,4 +1,4 @@
 const { exec } = require('child_process');
 const outs = require('../lib/exec-out');
 
-exec('npx office-addin-debugging start dist/manifest.xml --no-debug', outs);
+exec('echo testing testing', outs);
