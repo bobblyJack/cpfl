d46975484956057e90dc4f1779e2c41f7b4c9227
LG <lg@cpfl.com.au>
2024-11-28 10:19:18 +1030

    before rebuilding to a single drive map

--- src/graph/index.ts
+++ src/graph/index.ts
@@ -7,16 +7,59 @@ abstract class GraphItem {
     protected static get root(): keyof SharepointFolders {
         throw new Error('abstract root access');
     }
-    public static index: Map<string, DriveItem | GraphItem>;
-    public static async init(refresh: boolean = false) {
-        this.index = await deltaRequest(this.root, refresh);
-
-        // set up a hybrid map which, when reference, replaces the plain object reference with a class instance.
-        // could possibly get it to go up its parents line until it hits one of the 3 special folders, also.
-        // given itemReference includes the full path, it would be simple to check if that includes folder path
-        // that way it can instance as a correct subclass. neat!
-
+    public static get path() { // fetch root path
+        return CPFL.app.env.then((env) => env.site.folders[this.root]);
     }
+
+    private static _index: Map<string, DriveItem | GraphItem>;
+    public static get index(): Map<string, DriveItem | GraphItem> {
+        if (!this._index) {
+            throw new Error('drive unmapped');
+        }
+        return this._index;
+    }
+
+    public static async init(refresh: boolean = false) {
+        try {
+            this._index = await deltaRequest(this.root, refresh);
+        } catch (err) {
+            console.error('drive unmapped', err);
+        }
+    }
+
+    public static async open(id: string): Promise<GraphItem> {
+        try {
+            let item = this.index.get(id);
+            if (!item) {
+                throw new Error('item unmapped');
+            }
+            if (item instanceof this) {
+                return item;
+            }
+            if (!item.parentReference) {
+                throw new Error('null parent reference');
+            }
+            const itemPath = item.parentReference.path;
+            const rootPath = await this.path;
+            if (!itemPath.includes(rootPath)) {
+                console.error(itemPath, rootPath);
+                throw new Error('invalid class opener');
+            }
+            switch (this.root) {
+                case "matters": return new MatterItem(id);
+                case "library": return new LibraryItem(id);
+                case "users": return new UserItem(id);
+            }
+        } catch (err) {
+            console.error('item unopened');
+            throw err;
+        }
+    }
+
+    // set up a hybrid map which, when reference, replaces the plain object reference with a class instance.
+    // could possibly get it to go up its parents line until it hits one of the 3 special folders, also.
+    // given itemReference includes the full path, it would be simple to check if that includes folder path
+    // that way it can instance as a correct subclass. neat!
 
     public readonly id: string;
     protected constructor(id: string) {
@@ -87,6 +130,9 @@ export class MatterItem extends GraphItem {
 
 export class LibraryItem extends GraphItem {
     public static readonly root: keyof SharepointFolders = "library";
+    public static async open(id: string): Promise<LibraryItem> {
+        return super.open(id);
+    }
 
 }
 
@@ -94,3 +140,8 @@ export class UserItem extends GraphItem {
     public static readonly root: keyof SharepointFolders = "users";
 
 }
+
+const test = GraphItem.open("test")
+const test2 = LibraryItem.open("test")
+const test3 = GraphItem.open<LibraryItem>("test");
+const test4 = LibraryItem.open<LibraryItem>("test");
